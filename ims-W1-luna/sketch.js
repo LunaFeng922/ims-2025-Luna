b89b98ca699f3c74d59bd1cfdf37047b373108b6
LunaFeng922 <159014802+LunaFeng922@users.noreply.github.com>
2025-03-21 01:57:02 -0400

    delete some pics

--- ims-W1-luna/sketch.js
+++ ims-W1-luna/sketch.js
@@ -1,7 +1,11 @@
+//ims-W1-Luna
+
 // Coding Train / Daniel Shiffman
 // Weighted Voronoi Stippling
 // https://thecodingtrain.com/challenges/181-image-stippling
-//orginal code: https://editor.p5js.org/codingtrain/sketches/Z_YV25_4G
+// original code: https://editor.p5js.org/codingtrain/sketches/Z_YV25_4G
+
+// I also add something for picture loading & canvas size to the code^^
 
 // All of the points
 let points = [];
@@ -19,23 +23,24 @@ function preload() {
 }
 
 function setup() {
+  //I added this part to let the sketch more ajustable to all the images being uploaded
   // Scale the image while keeping aspect ratio
   let maxSize = 800;
-  let reScale= maxSize / max(picCho.width, picCho.height);
+  let reScale = maxSize / max(picCho.width, picCho.height);
 
   // Resize the image
   imgWidth = int(picCho.width * reScale);
   imgHeight = int(picCho.height * reScale);
 
   picCho.resize(imgWidth, imgHeight);
-  
+
   //Same size as the picture being chosen
   createCanvas(imgWidth, imgHeight);
 
   // Generate random points avoiding bright areas
   // generateRandomPoints(6000);
-  // The line above is the original code, I want to have the number      of the points adjusting accoding to the picture size
-  generateRandomPoints(imgWidth*imgHeight/50);
+  // The line above is the original code, I want to have the number of the points adjusting accoding to the picture size so I modified it into the one below
+  generateRandomPoints((imgWidth * imgHeight) / 50);
 
   // Calculate Delaunay triangulation and Voronoi diagram
   delaunay = calculateDelaunay(points);
@@ -57,10 +62,14 @@ function generateRandomPoints(n) {
   for (let i = 0; i < n; i++) {
     let x = random(width);
     let y = random(height);
+    //Get the color of the point pos in orginal picture
     let col = picCho.get(x, y);
+    //About Alpha: Brighter part is more lightly to be abandoned, darker part is more likely to be got & stored
+    //Ex. random(100)=50, Point A bright(col)=25, got; Point B bright(col)=75, neglected
     if (random(100) > brightness(col)) {
       points.push(createVector(x, y));
     } else {
+      //try & try & try until reach the number of the points required
       i--;
     }
   }
@@ -93,13 +102,20 @@ function updatePoints() {
   let delaunayIndex = 0;
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
+      //i --> xpos, j --> ypos, through the calculation, we can get the rgba (4 in total) info of the corresponding pixel in the picture
       let index = (i + j * width) * 4;
       let r = picCho.pixels[index + 0];
       let g = picCho.pixels[index + 1];
       let b = picCho.pixels[index + 2];
+      //calculate brightness through rgb
       let bright = (r + g + b) / 3;
+      //determine whether it's bright or dark --> the weight of darkness
+      //darker --> weight higher
       let weight = 1 - bright / 255;
+      //determine which voronoi the pixel belongs to
       delaunayIndex = delaunay.find(i, j, delaunayIndex);
+      //AWWWW, I couldn't understand the lines below as for how they work, which I think is indeed the essential part of this sketch
+      //calculate & store the total weight inside a voronoi
       centroids[delaunayIndex].x += i * weight;
       centroids[delaunayIndex].y += j * weight;
       weights[delaunayIndex] += weight;
@@ -109,14 +125,19 @@ function updatePoints() {
   // Compute weighted centroids
   for (let i = 0; i < centroids.length; i++) {
     if (weights[i] > 0) {
+      //So as to how it works
+      //calculate the pos of the centroids according to the weight
       centroids[i].div(weights[i]);
     } else {
+      //no pixel inside the voronoi --> remain the original pos as points[i]
       centroids[i] = points[i].copy();
     }
   }
 
   // Interpolate points
   for (let i = 0; i < points.length; i++) {
+    //https://p5js.org/reference/p5/lerp/
+    //points[i] gradually (0.1) move to centroids
     points[i].lerp(centroids[i], 0.1);
   }
 
@@ -131,5 +152,6 @@ function calculateDelaunay(points) {
   for (let v of points) {
     pointsArray.push(v.x, v.y);
   }
+  //https://github.com/d3/d3-delaunay
   return new d3.Delaunay(pointsArray);
 }
